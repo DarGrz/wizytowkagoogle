f6aa8c1bd8273d0adafadce69c427096f20807ab
d.grzegorczyk@outlook.com <you@example.com>
2024-01-24 22:04:10 +0100

    Quote padding changed

--- app/components/Quote.tsx
+++ app/components/Quote.tsx
@@ -5,7 +5,7 @@ const Quote = ({quoteText} : {
     quoteText:any
 }) => {
   return (
-    <div className='container quote-style text-white text-center quote-bg-gradient-burbon py-10 mt-20 md:py-20 md:mt-40'>
+    <div className='container quote-style text-white text-center quote-bg-gradient-burbon py-10 px-5 mt-20 md:py-20 md:mt-40'>
      <q className='font-bold text-2xl md:text-4xl'>{quoteText}</q>
      <p className='opacity-30 font-extralight text-sm mt-1'>-Marek Nowacki</p>
      <div className='flex gap-2 justify-center mt-3'>
