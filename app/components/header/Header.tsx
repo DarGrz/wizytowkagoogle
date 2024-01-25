2821c2872949d78e1a0b4391794dc0d52fce0dd5
d.grzegorczyk@outlook.com <you@example.com>
2024-01-25 01:05:43 +0100

    WhatCards Styling Finished

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -5,9 +5,9 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <div className="container justify-between items py-24  md:flex">
+    <div className="container justify-between items py-12 md:py-24 mb-10  md:flex">
         <div className="flex flex-col justify-center gap-3 flex-1 z-10">
-            <h1 className="text-5xl text-center md:text-left md:text:6xl lg:text-7xl font-bold main-text-gradient">Twoja Wizytówka Google na Pierszym Miejscu</h1>
+            <h1 className="text-4xl text-center md:text-left md:text:6xl lg:text-7xl font-bold main-text-gradient">Twoja Wizytówka Google na Pierszym Miejscu</h1>
             <p className="text-sm md:text-xl text-center md:text-left">Zajmę się Twoją wizytówką Google zgodnie z najnowszymi zasadami.</p>
             
         </div>
