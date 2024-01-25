ab711dcbc3d47804febd1546427daae5d009babf
d.grzegorczyk@outlook.com <you@example.com>
2024-01-25 01:06:00 +0100

    WhatCards Styling Finished

--- app/components/what-cards/WhatCards.tsx
+++ app/components/what-cards/WhatCards.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Image from 'next/image'
 import ImageMoreClients from '@/public/images/jak-przyciagnac-klientow.png'
 import ImageDigitalMarketing from '@/public/images/reklama-google.png'
+import ImageOpinions from '@/public/images/opinie-google.png'
+import ImageVisibility from '@/public/images/wysoka-pozycja-w-goole.png'
 
 const WhatCards = () => {
   return (
@@ -17,15 +19,18 @@ const WhatCards = () => {
           <div className='sub-col flex flex-col gap-2 justify-center'>
           <div className="flex h-full gap-2">
             <div className="cards grid place-content-center border-2 rounded-sm md-p5 p-3 text-center drop-shadow-sm md:w-2/4">
-              <h3 className='uppercase text-md md:text-xl font-bold'>Większą widoczność w Google</h3>
+              <Image className='w-2/4 md:w-2/4 mx-auto'  src={ImageVisibility} height={250} width={250} alt={"lokalna widoczność w google"} />
+              <h3 className='uppercase text-md md:text-xl font-bold'>Wysoka widoczność w Google</h3>
             </div>
-            <div className="cards grid grid-inline place-content-center border-2 rounded-sm md-p5 p-3 text-center w-full drop-shadow-sm md:w-2/4">
-              <h3 className='text-md md:text-xl font-bold'>NOWE POZYTYWNE OPINIE</h3></div>
+            <div className="cards grid grid-inline place-content-center border-2 rounded-sm md-p5 p-3 text-center  drop-shadow-sm md:w-2/4">
+              <Image className='w-2/4 md:w-2/4 mx-auto'  src={ImageOpinions} height={250} width={250} alt={"lokalna widoczność w google"} />
+              <h3 className='text-md md:text-xl font-bold'>NOWE POZYTYWNE OPINIE</h3>
+              </div>
             </div>
             <div className="flex h-full">
                 <div className="cards flex items-center  border-2 rounded-sm  p-2 text-center  drop-shadow-sm w-full">
                   <h3 className='bottom-5 md:text-xl text:md uppercase w-40 md:w-80  rounded-md py-3 pl-10 font-bold'>Magnetyczne Kampanie Reklamowe</h3>
-                  <Image className='w-1/3 md:w-2/4 mx-auto'  src={ImageDigitalMarketing} height={250} width={250} alt={"lokalna widoczność w google"} />
+                  <Image className=' md:w-2/4 mx-auto'  src={ImageDigitalMarketing} height={250} width={250} alt={"lokalna widoczność w google"} />
                 </div>
             </div>
           </div>
